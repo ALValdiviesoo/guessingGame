0247f4ab991681c8dd48dfed567a15464b808f74
ALValdiviesoo <ledorvaldivieso@protonmail.com>
2023-02-01 11:29:50 +0800

    Added sound effects

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let answer = document.querySelector(".answer");
 let submit = document.querySelector(".submit");
 let reset = document.querySelector(".reset");
+const correct = document.getElementById("correct");
+const wrong = document.getElementById("wrong");
 
 submit.onclick = function() {
   let random = (Math.floor(Math.random() * 10)) + 1;
+
   if (answer.value == random) {
     document.body.style.backgroundColor = "green";
+    correct.play();
   }
   else if(answer.value != random) {
     document.body.style.backgroundColor = "red";
+    wrong.play();
   }
 }
 
